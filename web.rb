b6cf62e7936240e40cadd79cc938af1142393d33
marianosimone <marianosimone@gmail.com>
2013-05-11 16:04:38 -0300

    add basic bin view

--- web.rb
+++ web.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
 
 get '/' do
-  erb :index
+    erb :index, locals: {tachos: [1,2]}
+end
+
+get '/bin' do
+    class Bin
+        attr_reader :name
+        def initialize()
+            @name = 'pepe'
+        end
+    end
+    erb :bin, locals: {bin: Bin.new()}
+end
+
+get '/bin/:name' do
+    content_type :json
+    puts params[:name]
+    return  {time: 2, count: 1}.to_json
 end
