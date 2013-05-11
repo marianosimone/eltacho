d8b1bb6bc10ae8982cbcfe181164aa1014a09cac
marianosimone <marianosimone@gmail.com>
2013-05-11 16:28:07 -0300

    interactive server connection

--- web.rb
+++ web.rb
@@ -1,23 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
+require 'time'
 
 get '/' do
     erb :index, locals: {tachos: [1,2]}
 end
 
-get '/bin' do
-    class Bin
-        attr_reader :name
-        def initialize()
-            @name = 'pepe'
+get '/bin/:id.?:format?' do
+    if params[:format] == "json"
+        content_type :json
+        return  {time: Time.now, count: 1}.to_json
+    else
+        class Bin
+            attr_reader :name
+            def initialize()
+                @name = 'pepe'
+            end
         end
+        erb :bin, locals: {bin: Bin.new()}
     end
-    erb :bin, locals: {bin: Bin.new()}
-end
-
-get '/bin/:name' do
-    content_type :json
-    puts params[:name]
-    return  {time: 2, count: 1}.to_json
 end
