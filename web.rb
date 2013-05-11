bb28e67258c8226a96eb9626d7bfa9772ede87ea
Demian Ferreiro <epidemian@gmail.com>
2013-05-11 19:13:23 -0300

    Add useful helper method and awesome 404 page :stuck_out_tongue:

--- web.rb
+++ web.rb
@@ -12,8 +12,7 @@ get '/' do
 end
 
 get '/bin/:id' do
-  bin = Bin[params[:id]]
-  return 404 unless bin
+  bin = bin_by_id(params[:id])
   respond_to do |f|
     f.html { erb :bin, locals: {bin: bin} }
     f.json { bin.to_hash.to_json }
@@ -21,10 +20,18 @@ get '/bin/:id' do
 end
 
 post '/bin/:id/dispose', provides: :json do
-  id = params[:id]
+  bin = bin_by_id(params[:id])
   count = params[:count] || 1
-  bin = Bin[id]
-  return 404 unless bin
   Disposal.create(bin: bin, count: count, created_at: Time.now)
   bin.to_hash.to_json
+end
+
+not_found do
+  '<img src="http://httpcats.herokuapp.com/404.jpg">'
+end
+
+helpers do
+  def bin_by_id(id)
+    Bin[id] || halt(404)
+  end
 end
